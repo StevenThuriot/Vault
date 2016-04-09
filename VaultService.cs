50b4ac2354b88d3c2736bee4d88846ecaa3c3849
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2016-04-09 13:20:22 +0200

    Or run as a windows service

---
+++ VaultService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ServiceProcess;
+using Microsoft.Owin.Hosting;
+
+namespace Vault
+{
+    partial class VaultService : ServiceBase
+    {
+        private IDisposable _webApp;
+
+        public VaultService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _webApp = WebApp.Start<Startup>("http://+:1234");
+        }
+
+        protected override void OnStop()
+        {
+            if (_webApp != null)
+            {
+                _webApp.Dispose();
+                _webApp = null;
+            }
+        }
+    }
+}
